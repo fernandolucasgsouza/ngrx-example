1e43e1a4cadcf7c99b2ecf237d8eb11e86712744
fernandolucasgsouza <fernandolucass.2012@gmail.com>
2026-06-13 05:34:47 -0300

    ajustingt reactive component

--- src/app/home/components/list-product/list-product.ts
+++ src/app/home/components/list-product/list-product.ts
@@ -1,20 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { IProduct } from '../../../interfaces/product.interface';
+import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+
 import { LoadProducts } from '../../../store/product.actions';
+import { IProduct } from '../../../interfaces/product.interface';
+import { selectProducts } from '../../../store/product.selectors';
 
 @Component({
   selector: 'app-list-product',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './list-product.html',
-  styleUrl: './list-product.scss',
+  styleUrls: ['./list-product.scss'],
 })
 export class ListProduct implements OnInit {
-  @Input() products: IProduct[] = [];
+   private store = inject(Store);
 
-  constructor(private store: Store) {}
-
+  public products$: Observable<IProduct[]> = this.store.select(selectProducts);
+  
   ngOnInit(): void {
+    this.store.dispatch(LoadProducts());
   }
 
   public deleteProduct(id: string): void {
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,27 +1,14 @@
-import { AsyncPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component} from '@angular/core';
 
-import { IProduct } from '../interfaces/product.interface';
 import { ListProduct } from './components/list-product/list-product';
 import { CreateProduct } from './components/create-product/create-product';
-import { select, Store } from '@ngrx/store';
-import { selectProducts } from '../store/product.selectors';
-import { Observable } from 'rxjs';
-import { LoadProducts } from '../store/product.actions';
 
 @Component({
   selector: 'app-home',
-  imports: [AsyncPipe, CreateProduct, ListProduct],
+  imports: [CreateProduct, ListProduct],
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit {
-  private store = inject(Store);
-
-  public products$: Observable<IProduct[]> = this.store.select(selectProducts);
-
-  ngOnInit(): void {
-    this.store.dispatch(LoadProducts());
-  }
+export class HomeComponent {
 }
